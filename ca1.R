132be16aa3953e3ebf7e035846c02b2e2865197f
Jason <jadomican@gmail.com>
2018-02-14 01:08:52 +0000

    Add R script

---
+++ ca1.R
@@ -0,0 +1,8 @@
+# Enterprise Database Technologies
+# X00119321 Jason Domican
+
+
+# Read in data from the csv dataset
+health <- read.table(file = "C:/Users/jadom_000/Desktop/Labsheets and Slides/EDT/CardiologyRel.csv", header=TRUE, sep =",")
+
+
